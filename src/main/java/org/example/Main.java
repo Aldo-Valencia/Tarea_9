2b4987e3674321883824d136f16e01b8690efd14
Lenovo <avalenciasandoval90@gmail.com>
2023-05-02 13:58:42 -0600

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.IU.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola mundo AWT");
+
+
+    }
+}
